352eed327277dd8174c4c56a25845c872da64558
Dimitris Bousoulegkas <74494805+StaringLongingly@users.noreply.github.com>
2024-08-09 18:49:45 +0300

    Split workspaces!

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ cd yay-bin
 makepkg -si
 
 # Install the packages needed for the install
-yay -S tgpt neovim neovide hyprland sddm mako wl-clipboard easyeffects kmix mpvpaper cool-retro-term parsecd fff waterfox grimblast
+yay -S hyprwayland-scanner-git tgpt neovim neovide hyprland-git sddm mako wl-clipboard easyeffects kmix mpvpaper cool-retro-term parsecd fff waterfox grimblast
 
 # Rice up Neovim
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
@@ -16,6 +16,11 @@ git clone https://github.com/nicefaa6waa/genshin-sddm-theme.git
 cd genshin-sddm-theme
 bash install-sddm-theme.sh
 rm -r genshin-sddm-theme
+
+# Setup Hyprland split workspaces
+hyprpm update
+hyprpm add https://github.com/shezdy/hyprsplit
+hyprpm enable hyprsplit
 
 # Finally starting Hyprland
 Hyprland
