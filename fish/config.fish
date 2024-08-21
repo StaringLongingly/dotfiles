ac070798946cc4ef9b82344bf3334139ef8e4a4a
staringlongingly <dimbous@proton.me>
2024-08-21 21:17:39 +0300

    Unifetch conf & touchscreen support

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
-acpi
-alias ai tgpt
+unifetch --chafa ~/.config/hypr/lapis_x4.png
+alias unifetch 'unifetch --chafa ~/.config/hypr/lapis_x4.png'
+# acpi -i | sed 's/Battery 0://g' | lolcat
+alias ai 'tgpt --provider llama2'
 starship init fish | source
