336503846ba00dbc9bd3e28f50bdfa73a57360f5
StaringLongingly <dimbous@proton.me>
2025-04-03 14:09:00 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ sudo pacman -Sy --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
+rm -rf yay-bin
+
+cp * .config/
 
 yay --save --answerdiff None --answerclean None --removemake
 
@@ -10,7 +13,7 @@ yay --save --answerdiff None --answerclean None --removemake
 yay -S --needed xdg-desktop-portal-hyprland-git pkg-config kitty cpio cmake acpi hyprwayland-scanner-git tgpt neovim neovide hyprland-git sddm mako wl-clipboard easyeffects kmix mpvpaper cool-retro-term parsec-bin fff waterfox-bin grimblast hyprpaper sirula swayosd-git vesktop plocate
 
 # Nvidia Drivers (can disable)
-# yay -S nvidia-nouveau nvidia-utils nvidia-open-dkms lib32-vulkan-nouveau vulkan-headers mangohud gamemode
+yay -S nvidia-nouveau nvidia-utils nvidia-open-dkms lib32-vulkan-nouveau vulkan-headers mangohud gamemode
 
 # Rice up Neovim
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
