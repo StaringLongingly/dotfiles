7db69eba2d2f8c30ffe2a2e001a0d6ceb57ee19a
StaringLongingly <dimbous@proton.me>
2025-04-03 18:25:28 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,18 @@
 sudo pacman -Sy --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
-makepkg -si
+yes | makepkg -si
 rm -rf yay-bin
 
 cp * .config/
 
-yay --save --answerdiff None --answerclean None --removemake
+yes | yay --save --answerdiff None --answerclean None --removemake
 
 # Install the packages needed for the install
-yay -S --needed xdg-desktop-portal-hyprland-git pkg-config kitty cpio cmake acpi hyprwayland-scanner-git tgpt neovim neovide hyprland-git sddm mako wl-clipboard easyeffects kmix mpvpaper cool-retro-term parsec-bin fff waterfox-bin grimblast hyprpaper sirula swayosd-git vesktop plocate
+yes | yay -S --needed xdg-desktop-portal-hyprland-git pkg-config kitty cpio cmake acpi hyprwayland-scanner-git tgpt neovim neovide hyprland-git sddm mako wl-clipboard easyeffects kmix mpvpaper cool-retro-term parsec-bin fff waterfox-bin grimblast hyprpaper sirula swayosd-git vesktop plocate
 
 # Nvidia Drivers (can disable)
-yay -S nvidia-nouveau nvidia-utils nvidia-open-dkms lib32-vulkan-nouveau vulkan-headers mangohud gamemode
+yes | yay -S nvidia-nouveau nvidia-utils nvidia-open-dkms lib32-vulkan-nouveau vulkan-headers mangohud gamemode
 
 # Rice up Neovim
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
@@ -23,6 +23,7 @@ rm -rf ~/.config/nvim/.git
 hyprpm update
 hyprpm add https://github.com/shezdy/hyprsplit
 hyprpm enable hyprsplit
+hyprpm update
 
 # Finally starting Hyprland
 Hyprland
