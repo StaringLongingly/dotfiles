d785d94f85e3b8bb11549cbf120e16b52c1d337b
StaringLongingly <dimbous@proton.me>
2025-04-03 22:42:26 +0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,31 @@
 # Install yay
+echo "Installing yay"
 sudo pacman -Sy --needed git base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 rm -rf yay-bin
 
+echo "Copying config files"
 cp * .config/
 
 yay --save --answerdiff None --answerclean None --removemake
 
 # Install the packages needed for the install
+echo "Installing basic packages"
 yes | yay -S --needed xdg-desktop-portal-hyprland-git pkg-config kitty cpio cmake acpi hyprwayland-scanner-git tgpt neovim neovide hyprland-git sddm mako wl-clipboard easyeffects kmix mpvpaper cool-retro-term parsec-bin fff waterfox-bin grimblast hyprpaper sirula swayosd-git vesktop plocate
 
 # Nvidia Drivers (can disable)
+echo "Installing nvidia drivers"
 yes | yay -S nvidia-nouveau nvidia-utils nvidia-open-dkms lib32-vulkan-nouveau vulkan-headers mangohud gamemode
 
 # Rice up Neovim
+echo "Configuring Neovim"
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
 # Setup Hyprland split workspaces
+echo "Installing Hyprland plugins"
 hyprpm update
 hyprpm add https://github.com/shezdy/hyprsplit
 hyprpm enable hyprsplit
