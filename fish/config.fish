58d9bbc9b6ef76010ef09520de9a040ab0ee2a0b
StaringLongingly <dimbous@proton.me>
2025-04-11 15:11:15 +0300

    Fri Apr 11 03:11:15 PM EEST 2025

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,17 @@
+# Set up Aliases
 alias unifetch 'unifetch --chafa ~/.config/hypr/zero_square.png'
 alias hyprconf 'nvim ~/.config/hypr/hyprland.conf'
-unifetch
-# acpi -i | sed 's/Battery 0://g' | lolcat
+alias yay-clean 'yay --answerdiff None --answerclean All'
+alias plex 'QT_QPA_PLATFORM=xcb Plex & disown'
 alias ai 'tgpt --provider deepseek'
+alias silence 'killall -9'
+
+# Run Unifetch or Hyprland depending if the system just booted
+if killall Hyprland -0
+  unifetch
+else
+  Hyprland
+end
+
+# Make It look Pretty
 starship init fish | source
